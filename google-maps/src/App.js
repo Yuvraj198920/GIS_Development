44a8dceaaf624d03ba8d1fc017db029c466664b1
Yuvraj198920 <yuvvraaj0flying@gmail.com>
2023-09-18 21:30:51 +0530

    created style.js

--- google-maps/src/App.js
+++ google-maps/src/App.js
@@ -1,8 +1,25 @@
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+import PlaceDetails from "./components/PlaceDetails/PlaceDetails";
+
+import { CssBaseline, Grid } from "@mui/material";
+
 const App = () => {
   return (
-    <div>
-      Hello world
-    </div>
+    <>
+      <CssBaseline/>
+      <Header />
+      <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
   )
 }
 
