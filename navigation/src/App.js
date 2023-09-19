f6dacfd3b350a1f72be18b7707800a988b16855f
Yuvraj198920 <yuvvraaj0flying@gmail.com>
2023-09-19 21:43:46 +0530

    Created actual components

--- navigation/src/App.js
+++ navigation/src/App.js
@@ -1,8 +1,28 @@
 import Navbar from './components/Navbar';
+import About from './pages/About';
+import Home from './pages/Home';
+import Pricing from './pages/Pricing';
 
 function App() {
+  // console.log(window.location)
+  let Component
+  switch(window.location.pathname){
+    case "/":
+      Component = <Home />
+      break
+    case "/pricing":
+      Component = <Pricing />
+      break
+    case "/about":
+      Component = <About />
+      
+  }
   return (
+    <>
     <Navbar />
+    {Component}
+    </>
+    
   );
 }
 
