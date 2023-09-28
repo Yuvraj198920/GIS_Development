98f10825cfed9297c473694fbf77c6ac41fe9e9e
Yuvraj198920 <yuvvraaj0flying@gmail.com>
2023-09-28 21:22:44 +0530

    Trying to add css to navbar

--- google-maps/src/common/Navbar.js
+++ google-maps/src/common/Navbar.js
@@ -1,11 +1,16 @@
-
+import "./navbar.css"
 function Navbar() {
     return(
         <nav>
-            <ul>
-                <li>About</li>
-                <li>Services</li>
-                <li>Contact</li>
+            <div className="logo">
+                <i className="fas fa-mountain"></i>
+                <h4>Mountains</h4>
+            </div>
+            <ul className="navlinks">
+                <li className="links"><a href="#">Home</a></li>
+                <li className="links"><a href="#">About</a></li>
+                <li className="links"><a href="#">Services</a></li>
+                <li className="links"><a href="#">Contact Us</a></li>
             </ul>
         </nav>
     )
