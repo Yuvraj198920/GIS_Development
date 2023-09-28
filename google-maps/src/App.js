01206256c1df173b6470bf0f0131be952dab6766
Yuvraj198920 <yuvvraaj0flying@gmail.com>
2023-09-28 21:12:06 +0530

    Updating navbar

--- google-maps/src/App.js
+++ google-maps/src/App.js
@@ -4,25 +4,20 @@ import List from "./components/List/List";
 import Map from "./components/Map/Map";
 import PlaceDetails from "./components/PlaceDetails/PlaceDetails";
 
+import "./App.css"
 
 import { CssBaseline, Grid } from "@mui/material";
 const google = window.google;
 const App = () => {
   return (
-    <>
-    <Navbar />
-      <CssBaseline/>
-      <Header />
-      <Grid container spacing={3} style={{ width: '100%' }}>
-        <Grid item xs={12} md={4}>
-          <List />
-        </Grid>
+    <div>
+      <Navbar />
+      <div className="main">
+    
+    </div>
+    </div>
 
-        <Grid item xs={12} md={8}>
-          <Map />
-        </Grid>
-      </Grid>
-    </>
+    
   )
 }
 
